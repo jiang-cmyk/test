a4fd3288947ab96730c68a7b6c612fe6ae77ff7e
gaohang jiang <2630353402@qq.com>
2021-05-11 20:05:03 +0800

    1
    20:04

---
+++ A1.java
@@ -0,0 +1,17 @@
+package bean;
+public class Student{
+   private long iD;
+   private String name;
+public  Student{}
+
+   public void setID(long iD){
+      this.iD = iD; }
+
+   public long getID(){
+      return iD;}
+
+   public void setName(String name){
+      this.name = name; }
+
+      return name; }
+}
